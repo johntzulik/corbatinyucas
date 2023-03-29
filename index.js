bf2172eb6c9abc7631b048e6f1d36248b2643980
Juan Felipe Morales <john.tzulik@gmail.com>
2023-03-29 14:58:09 -0600

    Update for the presentation

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
+const fs = require("fs");
 var jimp = require('jimp');
 const QRCode = require('qrcode');
+var path = require('path')
+const dotenv = require("dotenv");
+
+dotenv.config({ path: path.join(__dirname, ".env") });
+
+if(!fs.existsSync(process.env.QR_FOLDER)) {
+    fs.mkdirSync(process.env.QR_FOLDER, { recursive: true });
+}else{
+    fs.rmSync(process.env.QR_FOLDER, { recursive: true });
+    fs.mkdirSync(process.env.QR_FOLDER, { recursive: true });
+}
+if(!fs.existsSync(process.env.OUTPUT_FOLDER)) {
+    fs.mkdirSync(process.env.OUTPUT_FOLDER, { recursive: true }); 
+}else{
+    fs.rmSync(process.env.OUTPUT_FOLDER, { recursive: true });
+    fs.mkdirSync(process.env.OUTPUT_FOLDER, { recursive: true }); 
+}
 
 const generateQR = async (casa,url) => {
 	try {
         var options = {width: 400,height: 400, margin: 1};
-        await QRCode.toFile('qrimages/'+casa+'.png', url, options);
+        await QRCode.toFile(process.env.QR_FOLDER+'/'+casa+'.png', url, options);
 	} catch(err){
 		console.log(err);
 	}
 }
 
 async function setQRtoCorbatin(casa) {
-    const image = await jimp.read('images/las-Yucas-corbatin-9x13.jpg');
+    const image = await jimp.read(process.env.SRC_FOLDER+'/'+process.env.BASE_IMAGE);
     //Agrega el texto de la casa
     const font = await jimp.loadFont(jimp.FONT_SANS_64_BLACK);
     image.print(font, 443,740, casa);
     //lee el codigo QR
-    const qrcode = await jimp.read('qrimages/'+casa+'.png');
+    const qrcode = await jimp.read(process.env.QR_FOLDER+'/'+casa+'.png');
     //agrega el codigo QR
     image.composite(qrcode, 332,810);
     //Guarda
-    await image.writeAsync('output/'+casa+'.jpg');
+    await image.writeAsync(process.env.OUTPUT_FOLDER+'/'+casa+'.jpg');
 }
 
-const casas = require("./json/casas");
+const casas = require(process.env.JSON_PATH);
 Promise.all(casas.map(
     async (casa) => {
         var numero = casa.casa
--- indexpdf.js
+++ indexpdf.js
@@ -1,7 +1,17 @@
 const { PDFDocument, PageSizes } = require("pdf-lib");
 const { writeFileSync, readFileSync } = require("fs");
 const fs = require("fs");
-const casas = require("./json/casas");
+const dotenv = require("dotenv");
+var path = require('path')
+dotenv.config({ path: path.join(__dirname, ".env") });
+const casas = require(process.env.JSON_PATH);
+
+if(!fs.existsSync(process.env.PDF_FOLDER)) {
+  fs.mkdirSync(process.env.PDF_FOLDER, { recursive: true });
+}else{
+  fs.rmSync(process.env.PDF_FOLDER, { recursive: true });
+  fs.mkdirSync(process.env.PDF_FOLDER, { recursive: true });
+}
 
 async function createPDF() {
   const document = await PDFDocument.create();
@@ -9,50 +19,60 @@ async function createPDF() {
   var contador = 1;
   var i = 0;
   var j = 1;
+  var borderleft = 0;
+  var borderbottom = 19;
   var page = document.addPage(PageSizes.Tabloid);
   for (const { id, casa } of casas) {
-    const imgBuffer = fs.readFileSync(`./output/${casa}.jpg`);
+    const imgBuffer = fs.readFileSync(`./${process.env.OUTPUT_FOLDER}/${casa}.jpg`);
     const img = await document.embedJpg(imgBuffer);
 
     const { width, height } = img.scale(0.23);
-    const imgdim = img.scale(0.228);
-    const xwidth = (page.getWidth() / 3) * j - width - 10;
-    const yheight = page.getHeight() - height * i - height - 20;
+    const imgdim = img.scale(0.23);
+    const xwidth = (page.getWidth() / 3) * j - width - borderleft;
+    const yheight = page.getHeight() - height * i - height - borderbottom;
     page.drawImage(img, {
       x: xwidth,
       y: yheight,
       width: imgdim.width,
       height: imgdim.height,
     });
-    console.log("xwidth: " + xwidth + "   yheight: " + yheight);
     console.log("C:" + contador + " i:" + i + "  j: " + j);
+    //console.log("xwidth: " + xwidth + "   yheight: " + yheight);
+    console.log("borderbottom: " + borderbottom);
 
     switch (contador) {
-      case 1:
-        break;
+      case 2:
+      case 5:
+      case 8:
+      borderleft = 38;
+      borderbottom=19+i;
+      break;
       case 3:
-        i += 1;
-        j = 0;
-        break;
       case 6:
+        borderleft = 0;
         i += 1;
         j = 0;
+        borderbottom=19+i;
         break;
       case 9:
+        borderleft = 0;
         i = 0;
         j = 0;
+        borderbottom=19+i;
         contador = 0;
         var page = document.addPage(PageSizes.Tabloid);
         console.log("-----------------------------------------");
         break;
       default:
+      borderleft = 19;
+      borderbottom=19+i;
         break;
     }
 
     j += 1;
     contador += 1;
   }
-  await writeFileSync(`./PDF/All-the-pages.pdf`, await document.save());
+  await writeFileSync(`./${process.env.PDF_FOLDER}/${process.env.NAME_PDF_FILE}`, await document.save());
 }
 
 createPDF().catch((err) => console.log(err));
